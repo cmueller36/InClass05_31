c2966259e1e66928ae1e5058fb602faa97a8842d
Christopher Mueller <christopher.mueller@cpxprojects.com>
2018-05-31 19:30:42 -0400

    initilize databse

---
+++ javascript.js
@@ -0,0 +1,18 @@
+//initialize databse
+
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyDLzUT4UfZkHRCLTkxvaGN9ugOI7XIuoJg",
+    authDomain: "inclass0531.firebaseapp.com",
+    databaseURL: "https://inclass0531.firebaseio.com",
+    projectId: "inclass0531",
+    storageBucket: "",
+    messagingSenderId: "145760898899"
+  };
+
+  firebase.initializeApp(config);
+
+  var database = 
+
+
+  database.ref().firebase
