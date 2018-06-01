caec79bb68301d509690e29a6068425c87f9eae1
Christopher Mueller <christopher.mueller@cpxprojects.com>
2018-05-31 20:57:09 -0400

    added moment

--- javascript.js
+++ javascript.js
@@ -2,12 +2,12 @@
 
 // Initialize Firebase
 var config = {
-    apiKey: "AIzaSyDLzUT4UfZkHRCLTkxvaGN9ugOI7XIuoJg",
-    authDomain: "inclass0531.firebaseapp.com",
-    databaseURL: "https://inclass0531.firebaseio.com",
-    projectId: "inclass0531",
-    storageBucket: "",
-    messagingSenderId: "145760898899"
+  apiKey: "AIzaSyDLzUT4UfZkHRCLTkxvaGN9ugOI7XIuoJg",
+  authDomain: "inclass0531.firebaseapp.com",
+  databaseURL: "https://inclass0531.firebaseio.com",
+  projectId: "inclass0531",
+  storageBucket: "",
+  messagingSenderId: "145760898899"
 };
 
 firebase.initializeApp(config);
@@ -20,50 +20,62 @@ var userDate = "";
 var userRate = "";
 var currentDate = "";
 var userBilled = "";
+var monthDiff = "";
+
+// var currentDateFormat = "DD/MM/YY";
+// var convertedDate = moment(a, currentDateFormat);
+// currentDate = convertedDate;
 
 
+// moment().format('MMMM Do YYYY');
+// moment(currentDate).toNow();
+// moment(userDate).diff(moment(), "months").toNow(); <-- calculates the diff between userDate and the present in months
 
 $("#submitButton").on("click", function (event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    userName = $("#newEmployeeID").val().trim();
-    userRole = $("#newRoleID").val().trim();
-    userDate = $("#newDateID").val().trim();
-    userRate = $("#newRateID").val().trim();
-    currentDate = new Date($.now());
+  userName = $("#newEmployeeID").val().trim();
+  userRole = $("#newRoleID").val().trim();
+  userDate = $("#newDateID").val().trim();
+  userRate = $("#newRateID").val().trim();
 
-    console.log(userName);
-    console.log(userRole);
-    console.log(userDate);
-    console.log(userRate);
-    console.log(currentDate);
+var userDatePretty = moment.unix(userDate).format("MM/DD/YYYY");
+// var monthsWorked = moment().diff(moment(userDate, "X"), "months");
+  monthDiff = moment(userDate).diff(moment(),"months");
 
-    database.ref().push({
-        EmployeeName: userName,
-        Role: userRole,
-        StartDate: userDate,
-        Rate: userRate,
-        SubmitDate: currentDate
+  console.log(userName);
+  console.log(userRole);
+  console.log(userDate);
+  console.log(userRate);
+  console.log(currentDate);
+  console.log(monthDiff);
 
-    });
+  database.ref().push({
+    EmployeeName: userName,
+    Role: userRole,
+    StartDate: userDate,
+    Rate: userRate,
+    SubmitDate: currentDate
 
-    $("#newEmployeeID").val('');
-    $("#newRoleID").val('');
-    $("#newDateID").val('');
-    $("#newRateID").val('');
+  });
+
+  $("#newEmployeeID").val('');
+  $("#newRoleID").val('');
+  $("#newDateID").val('');
+  $("#newRateID").val('');
 
 });
 
 
 
-  //grab the added information
-  database.ref().on("child_added", function(childSnaphot){
-     console.log(childSnaphot.val().EmployeeName);
-     console.log(childSnaphot.val().Role);
-     console.log(childSnaphot.val().StartDate);
-     console.log(childSnaphot.val().Rate);
-     console.log(childSnaphot.val().SubmitDate);
+//grab the added information
+database.ref().on("child_added", function (childSnaphot) {
+  console.log(childSnaphot.val().EmployeeName);
+  console.log(childSnaphot.val().Role);
+  console.log(childSnaphot.val().StartDate);
+  console.log(childSnaphot.val().Rate);
+  console.log(childSnaphot.val().SubmitDate);
 
-     //add new rows to table
-     $()
-  });
+  //add new rows to table
+  $()
+});
