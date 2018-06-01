f08200826fb502d5e4fddfcb777ef18bb0235c76
Christopher Mueller <christopher.mueller@cpxprojects.com>
2018-05-31 20:18:32 -0400

    updated js

--- javascript.js
+++ javascript.js
@@ -14,33 +14,56 @@ firebase.initializeApp(config);
 
 var database = firebase.database();
 
+var userName = "";
+var userRole = "";
+var userDate = "";
+var userRate = "";
+var currentDate = "";
+var userBilled = "";
 
 
 
 $("#submitButton").on("click", function (event) {
     event.preventDefault();
-    var userName = $("#newEmployeeID").val().trim();
-    var userRole = $("#newRoleID").val().trim();
-    var userDate = $("#newDateID").val().trim();
-    var userRate = $("#newRateID").val().trim();
+
+    userName = $("#newEmployeeID").val().trim();
+    userRole = $("#newRoleID").val().trim();
+    userDate = $("#newDateID").val().trim();
+    userRate = $("#newRateID").val().trim();
+    currentDate = new Date();
 
     console.log(userName);
     console.log(userRole);
     console.log(userDate);
     console.log(userRate);
+    console.log(currentDate);
 
     database.ref().push({
         EmployeeName: userName,
         Role: userRole,
         StartDate: userDate,
-        Rate: userRate
+        Rate: userRate,
+        SubmitDate: currentDate
+
     });
+
+    $("#newEmployeeID").val('');
+    $("#newRoleID").val('');
+    $("#newDateID").val('');
+    $("#newRateID").val('');
 
 });
 
 
 
-//   //grab the added information
-//   databse.ref().on("child_added", function(snaphot){
-//      var sv = snapshot.val(); 
-//   });
+  //grab the added information
+  database.ref().on("child_added", function(childSnaphot){
+     console.log(childSnaphot.val().EmployeeName);
+     console.log(childSnaphot.val().Role);
+     console.log(childSnaphot.val().StartDate);
+     console.log(childSnaphot.val().Rate);
+     console.log(childSnaphot.val().SubmitDate);
+
+     //add new rows to table
+     $()
+  });
